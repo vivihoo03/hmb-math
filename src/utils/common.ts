bc450fbe8822cf792ff618dfcaafbc9d95299fed
ken <13913513385@163.com>
2020-02-11 17:52:03 +0800

    test lint

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,7 +1,6 @@
 import { DefaultDisTorlance } from '../constants';
 import { HMath } from '../math/math';
 
-
 export function nearlyEqual(a: number, b: number, torlance = DefaultDisTorlance) {
     return HMath.nearlyEqual(a, b, torlance);
 }
